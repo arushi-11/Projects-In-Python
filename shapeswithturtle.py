80f4b97be81fdc13d15e14d253014ff54037f018
arushi1127 <emarushi08@gmail.com>
2020-10-22 01:57:21 +0530

    pushed

---
+++ shapeswithturtle.py
@@ -0,0 +1,17 @@
+import turtle
+x = turtle.Turtle()
+x.begin_fill()
+x.fillcolor("red")
+for i in range(4):
+    x.forward(100)
+    x.left(90)
+x.end_fill()    
+
+
+# x.circle(50)
+# x.hideturtle()-  it hides the pointer!
+# Forward() - Defines the distance to be moved in the forward direction.
+# Backward() - Defines the distance to be moved in the backward direction!
+# Left() - Defines the angle with which we have to bent our pointer in left direction.
+# Right() - Defines the angle with which we have to bent our pointer in right direction.
+
---
+++ turtledesign.py
@@ -0,0 +1,14 @@
+# making a square with turtle!
+
+import turtle
+x= turtle.Turtle()
+x.shape("turtle")
+x.forward(100)
+x.left(90)
+x.forward(100)
+x.left(90)
+x.forward(100)
+x.left(90)
+x.forward(100)
+
+
